b85b61fd969f0a9a6ea810ad5cf400e39eb4fa41
Helana <helanan@gmail.com>
2017-02-01 11:56:54 -0600

    added basic jquery vs. javascript syntax

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# NSS Introduction to jQuery
+#Introduction to jQuery
 ##Basic syntax and overview of how to implement jQuery into code over vanilla javascript
+
+###Before you begin: 
+
+- make sure jquery library has loaded and document is ready
+- using cdn but can also use npm install
+
+####Basics
+
+- jQuery *always* returns an array of elements, EVEN when theres only one
+	- therefore always need to specify which array object to select with selector for ex: [0]
+
+#####Selecting By Tag Name
+- $("button");
+- button.html();
+
+#####Selecting By Tag Name
+- $(".element--name").each(function(arrayIndex, currentElement){});
+- output: arrayIndex & current html element
+	-Index values of items
+
+#####Creating Your Own Attributes
+- can create any attribute in jQuery by selecting its parent element and its assigned attribute
+
+#####Find an element
+- .find
+
+#####AJAX Calls
+- $.ajax ({url: "#"}).done(functionIWantjQueryToExecute);
+- function functionIWantjQueryToExecute(item) {for loop {currentItem = itemList.items[i]; $("#elementName").append("html element" + item.attribute);}}
---
+++ javascripts/main.js
@@ -0,0 +1,77 @@
+console.log("hello jqueryStarter Script");
+
+$(document).ready(function () {
+
+//vanilla javascript example: 
+var topArticle = document.getElementById("top-article");
+
+//jquery sytnax below: 
+var article = $("#top-article");
+
+console.log("topArticle", topArticle);
+console.log("article", article[0]);
+
+		/////select by tag name/////
+
+//vanilla javascript example:
+var allButtons = document.getElementsByTagName("button");
+
+//jquery syntax below:
+var button = $("button");
+console.log("button", button, button.html());
+
+		/////select by class/////
+
+//vanilla javascript example:
+var mainArticles = document.getElementsByClassName("article--main");
+console.log("mainArticles", mainArticles);
+
+//jquery syntax below:
+$(".article--main").each(function(arrayIndex, currentElement) {
+	console.log(arrayIndex, $(currentElement).html());
+	});
+
+		//////Select by Attribute/////
+var umbrellaElement = $("h1[umbrella='open']");
+console.log("umbrella text: ", umbrellaElement.html());
+
+//Select children
+console.log("all children: ", $(".container").children()); //There are two children elements
+
+//Select specific children
+console.log("the h1: ", $(".container").children("h1"));
+console.log("the section: ", $(".container").children("section"));
+
+//Select specific child index (useful for ul/ol)
+//Example of chaining selectors
+console.log("third li: ", $("#letters").children(".letter").eq(2).html());
+
+//Finding parent DOM elements
+console.log($(".album").parents(".song-container").attr("index"));
+
+		/////Finding an element////
+var album = $(".container").find(".album");
+console.log("album", album.html());
+
+//jquery get my list of songs
+function functionIWantjQueryToExecute(songList) {
+      console.log(songList.songs);
+
+      for (var i = 0; i < songList.songs.length; i++) {
+        var currentSong = songList.songs[i];
+        $("#list-of-songs").append("<h1>"+ currentSong.title +"</h1>");
+        $("#list-of-songs").append("<div>Performed by " + currentSong.artist + "</div>");
+        $("#list-of-songs").append("<div>On the album " + currentSong.album + "</div>");
+      }
+    }
+
+//ajax call
+$.ajax({
+	url:"javascripts/songs.json"
+}).done(functionIWantjQueryToExecute);
+
+
+
+
+
+});
